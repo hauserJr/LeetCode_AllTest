2f3fc3a2894fc3e83f4a7df7f47bcfd6b90f2174
hauserJr <free576002@gmail.com>
2017-08-17 00:08:53 +0800

    20170817

--- LeetCode.cs
+++ LeetCode.cs
@@ -7,10 +7,9 @@ public class LeetCode_AllTest
 {
     static void Main(string[] args)
     {
-        
     }
 
-    #region RunTime Beats 55.74% in LeetCode , Level：Easy
+    #region RunTime(142 ms) Beats 55.74% in LeetCode ,Level：Easy
     public static bool JudgeCircle(string moves)
     {
         int[] Ordinate = new int[] { 0, 0 };
@@ -37,5 +36,32 @@ public class LeetCode_AllTest
         return OriPath;
     }
     #endregion
+
+    #region RunTime(59 ms) Beats 73.77%  in LeetCode ,,Level：Easy
+    public static int HammingDistance(int x , int y)
+    {
+        int XOR_Value = x ^ y;
+        int AND_Value = 1;
+        int result = 0;
+        while (XOR_Value != 0)
+        {
+            if ((AND_Value & XOR_Value) == 1)
+            {
+                result++;
+            }
+            XOR_Value = XOR_Value >> 1;
+        }
+        return result;
+    }
+
+  
+    #endregion
+
 }
 
+
+//SQL 語法
+#region RunTime Beats(2962 ms) 73.64% in LeetCode , Level：Easy
+//Big Countries 
+//Ans：SELECT name,population,area FROM World WHERE population > 25000000 OR area > 3000000
+#endregion
