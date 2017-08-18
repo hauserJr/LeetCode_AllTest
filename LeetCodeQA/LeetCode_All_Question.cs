5f92ae6bbb78aa85165df670012d622d385385e8
hauserJr <free576002@gmail.com>
2017-08-18 15:09:33 +0800

    20170818
    調整

--- LeetCodeQA/LeetCode_All_Question.cs
+++ LeetCodeQA/LeetCode_All_Question.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public class LeetCode_AllTest
-{
-    static void Main(string[] args)
-    {
-    }
 
+
+public class LeetCode_All_Question
+{
     #region RunTime(142 ms) Beats 55.74% in LeetCode ,Level：Easy
-    public static bool JudgeCircle(string moves)
+    public bool JudgeCircle(string moves)
     {
         int[] Ordinate = new int[] { 0, 0 };
         foreach (var item in moves)
@@ -27,7 +25,7 @@ public class LeetCode_AllTest
             {
                 Ordinate[0] = Ordinate[0] - 1;
             }
-            else 
+            else
             {
                 Ordinate[0] = Ordinate[0] + 1;
             }
@@ -37,8 +35,8 @@ public class LeetCode_AllTest
     }
     #endregion
 
-    #region RunTime(59 ms) Beats 73.77%  in LeetCode ,,Level：Easy
-    public static int HammingDistance(int x , int y)
+    #region RunTime(59 ms) Beats 73.77%  in LeetCode ,Level：Easy
+    public int HammingDistance(int x, int y)
     {
         int XOR_Value = x ^ y;
         int AND_Value = 1;
@@ -52,11 +50,17 @@ public class LeetCode_AllTest
             XOR_Value = XOR_Value >> 1;
         }
         return result;
-    } 
+    }
+    #endregion
+
+    #region RunTime(2962 ms) Beats 73.64% in LeetCode , Level：Easy
+    public string BigCountries()
+    {
+        string SqlResult = string.Format("SELECT name,population,area FROM World WHERE population > 25000000 OR area > 3000000");     
+        return SqlResult;
+    }
     #endregion
 }
-    //SQL 語法
-    #region RunTime Beats(2962 ms) 73.64% in LeetCode , Level：Easy
-    //Big Countries 
-    //Ans：SELECT name,population,area FROM World WHERE population > 25000000 OR area > 3000000
-    #endregion
+
+
+
---
+++ LeetCodeResultShow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LeetCodeResultShow
+{
+    static System.Diagnostics.Stopwatch RunTime = new System.Diagnostics.Stopwatch();
+    static string RunTime_ms = string.Empty;
+    static void Main(string[] args)
+    { 
+        //Question Area , 題目集中區
+        LeetCode_All_Question _CallResult = new LeetCode_All_Question();
+
+        #region 取得程式執行時間,非LeetCode上RunTime
+        RunTimeSetUp();
+        #endregion
+
+        #region 程式呼叫區,Method Call
+        
+        #endregion
+
+        #region 停止計算程式執行時間
+        RunTime.Stop();
+        RunTime_ms = string.Format(@"{0}ms",RunTime.Elapsed.TotalMilliseconds.ToString());
+        #endregion
+
+        //結果顯示
+        Console.Write("RunTime(Method/ms): " + RunTime_ms);
+        Console.Read();
+
+    }
+
+    #region Run Time Process
+    private static void RunTimeSetUp()
+    {
+        RunTime.Reset();//碼表歸零
+        RunTime.Start();//碼表開始計時
+    }
+    #endregion
+}
